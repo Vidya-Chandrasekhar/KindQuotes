f8027ee70a8397052d90560f5f66e92f5f0612b1
vidyachandasekhar <vidyahari.vs@Gmail.com>
2017-06-25 15:44:58 -0400

    implemented GET without specifying ID, i.e get all inputs in JSon Array form.

--- server/src/main/java/io/zipcoder/kindquotes/controller/QuoteController.java
+++ server/src/main/java/io/zipcoder/kindquotes/controller/QuoteController.java
@@ -9,6 +9,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 
 @RequestMapping("/")
 @RestController
@@ -20,7 +24,7 @@ public class QuoteController {
     @Autowired
     QuoteRepository quoteRepository; //Service which will do all data retrieval/manipulation work
 
-
+    //GET method - reads
     @RequestMapping(value = "/quote/{id}", method = RequestMethod.GET)
     public ResponseEntity<?> getQuote(@PathVariable("id") long id) {
         logger.info("Fetching User with id {}", id);
@@ -32,26 +36,37 @@ public class QuoteController {
         }
         return new ResponseEntity<>(quote, HttpStatus.OK);
     }
+    // GET all, without specifying ID
+    @RequestMapping(value = "/quote/", method = RequestMethod.GET)
+    public ResponseEntity<?> getQuote() {
+        logger.info("Fetching all quotes");
+        List<Quote> quoteList  = (List<Quote>) quoteRepository.findAll();
+        // this return an iterable  which is a super interface for list, so we can safely cast to a list
+        return new ResponseEntity<>(quoteList, HttpStatus.OK);
+    }
 
+    // POST create an input
     @RequestMapping(value = "/quote/", method = RequestMethod.POST)
     public ResponseEntity<?> createQuote(@RequestBody Quote quote) {
         logger.info("Creating quote : {}", quote);
         quoteRepository.save(quote);
-        return new ResponseEntity<>( quote,HttpStatus.CREATED);
+        return new ResponseEntity<>(quote, HttpStatus.CREATED);
     }
 
+    // DELETE an input
     @RequestMapping(value = "/quote/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<?> deleteQuote(@PathVariable("id") long id) {
         logger.info("Deleting User with id {}", id);
         quoteRepository.delete(id);
 
-        return new ResponseEntity<>( HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    // PUT updates an input
     @RequestMapping(value = "/quote/", method = RequestMethod.PUT)
     public ResponseEntity<?> updateQuote(@RequestBody Quote quote) {
         logger.info("Updating quote : {}", quote);
         quoteRepository.save(quote);
-        return new ResponseEntity<>( quote,HttpStatus.CREATED);
+        return new ResponseEntity<>(quote, HttpStatus.CREATED);
     }
 }
